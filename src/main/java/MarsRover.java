d7ccd1ed50b5680dd450c28e5876104727a176d9
Joseph Ding <dingyiheng91@gmail.com>
2025-08-24 18:25:49 +0800

    feat: 加入rover向北移动一步能力

--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -1,2 +1,22 @@
 public class MarsRover {
+    private int x;
+    private int y;
+    private String direction;
+    public MarsRover(int x, int y, String direction) {
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+
+    }
+    public void execute(String command) {
+        if (command.equals("M")) {
+            if (this.direction.equals("N")) {
+                this.y += 1;
+            }
+        }
+    }
+
+    public String report() {
+        return "(" + this.x + ", " +  this.y + ")" + " " + this.direction;
+    }
 }
