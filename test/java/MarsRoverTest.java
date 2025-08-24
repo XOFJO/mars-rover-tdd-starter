46d01be0582a484111565833fbc627862e78e63a
Joseph Ding <dingyiheng91@gmail.com>
2025-08-24 18:14:44 +0800

    test: 加入基础测试（面向北面向前移动一步)

--- test/java/MarsRoverTest.java
+++ test/java/MarsRoverTest.java
@@ -1,2 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MarsRoverTest {
+    @Test
+    public void test_move_forward_facing_north() {
+        // Given
+        MarsRover rover = new MarsRover(0, 0, "N");
+
+        // When
+        rover.execute("M");
+
+        // Then
+        assertEquals("(0, 1) N", rover.report());
+    }
 }
+
+
+
