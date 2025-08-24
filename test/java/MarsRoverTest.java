9c5893ec43af1e207022b2550b476469eb23ac09
Joseph Ding <dingyiheng91@gmail.com>
2025-08-24 18:30:42 +0800

    test: 加入测试用例（所有方向的前进一步）“
    ”

--- test/java/MarsRoverTest.java
+++ test/java/MarsRoverTest.java
@@ -14,6 +14,46 @@ public class MarsRoverTest {
         // Then
         assertEquals("(0, 1) N", rover.report());
     }
+
+
+    @Test
+    public void test_move_forward_facing_south() {
+        // Given
+        MarsRover rover = new MarsRover(0, 0, "S");
+
+        // When
+        rover.execute("M");
+
+        // Then
+        assertEquals("(0, -1) S", rover.report());
+    }
+
+
+    @Test
+    public void test_move_forward_facing_west() {
+        // Given
+        MarsRover rover = new MarsRover(0, 0, "W");
+
+        // When
+        rover.execute("M");
+
+        // Then
+        assertEquals("(-1, 0) W", rover.report());
+    }
+
+
+    @Test
+    public void test_move_forward_facing_East() {
+        // Given
+        MarsRover rover = new MarsRover(0, 0, "E");
+
+        // When
+        rover.execute("M");
+
+        // Then
+        assertEquals("(1, 0) E", rover.report());
+    }
+
 }
 
 
